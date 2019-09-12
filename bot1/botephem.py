6c4f20cce0cfbda888f238643e0f32ce3b64be87
Mansur Arsanukaev <arsanukaev.m@yandex.ru>
2019-09-13 01:10:07 +0300

    Add planet bot telegram

--- bot1/botephem.py
+++ bot1/botephem.py
@@ -1,13 +1,75 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import ephem
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080','urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 def hello(bot, update):
-    print('Привет')
+    print('Привет!')
+    update.message.reply_text("Привет")
+    
+def mars(bot, update):
+    myplanet = update.message.text
+    print(myplanet)
+    if myplanet == "Mars":
+        mymars = ephem.Mars('2019/09/12')
+        update.message.reply_text(f'{mymars}')
+        const = ephem.constellation(mymars)
+        update.message.reply_text(f'{const}')
+    elif myplanet == "Mercury":
+        myMercury = ephem.Mercury('2019/09/12')
+        update.message.reply_text(f'{myMercury}')
+        const2 = ephem.constellation(myMercury)
+        update.message.reply_text(f'{const2}')
+    elif myplanet == "Venus":
+        myVenus = ephem.Venus('2019/09/12')
+        update.message.reply_text(f'{myVenus}')
+        const3 = ephem.constellation(myVenus)
+        update.message.reply_text(f'{const3}')
+    elif myplanet == "Jupiter":
+        myJupiter = ephem.Jupiter('2019/09/12')
+        update.message.reply_text(f'{myJupiter}')
+        const4 = ephem.constellation(myJupiter)
+        update.message.reply_text(f'{const4}')
+    elif myplanet == "Saturn":
+        mySaturn = ephem.Saturn('2019/09/12')
+        update.message.reply_text(f'{mySaturn}')
+        const5 = ephem.constellation(mySaturn)
+        update.message.reply_text(f'{const5}')
+    elif myplanet == "Uranus":
+        myUranus = ephem.Uranus('2019/09/12')
+        update.message.reply_text(f'{myUranus}')
+        const6 = ephem.constellation(myUranus)
+        update.message.reply_text(f'{const6}')
+    elif myplanet == "Neptune":
+        myNeptune = ephem.Neptune('2019/09/12')
+        update.message.reply_text(f'{myNeptune}')
+        const7 = ephem.constellation(myNeptune)
+        update.message.reply_text(f'{const7}')
+    elif myplanet == "Pluto":
+        myPluto = ephem.Pluto('2019/09/12')
+        update.message.reply_text(f'{myPluto}')
+        const8 = ephem.constellation(myPluto)
+        update.message.reply_text(f'{const8}')
+    else: 
+        update.message.reply_text("Нет такой планеты!")
+
+
+
+
+def help(bot, update):
+    update.message.reply_text('Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto')
 
 def main():
-    mybot = Updater('808049629:AAHXH6XLn_2HpMGsgUBn9y0TCI2mn22mJ8A', request_kwargs=PROXY)
+    mybot = Updater('808049629:AAHXH6XLn_2HpMGsgUBn9y0TCI2mn22mJ8A')
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", hello))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, mars))
+   
     mybot.start_polling()
     mybot.idle()
 main()
